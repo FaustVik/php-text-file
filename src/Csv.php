5f82a76c53c1aa6172e1f38bded548ac717bf8d1
viktor <victor_tech@1td.eu>
2021-07-28 13:41:15 +0300

    #hotfix, added methods write to csv

--- src/Csv.php
+++ src/Csv.php
@@ -46,7 +46,7 @@ class Csv extends AbstractFile
 
         $counter = 0;
 
-        if (($handle = fopen($this->path_to_file, 'rb')) !== false) {
+        if (($handle = $this->openFile()) !== false) {
             while (($data = fgetcsv($handle, $length, $separator, $enclosure, $escape)) !== false) {
                 if ($counter === 0 && $this->skip_first_line) {
                     $counter++;
@@ -108,5 +108,92 @@ class Csv extends AbstractFile
     {
         $this->skip_first_line = true;
         return $this;
+    }
+
+    /**
+     * @param string $text
+     * @param string $separator
+     *
+     * @return array
+     */
+    public function stringToArray(string $text, string $separator): array
+    {
+        if ($text === '' || $separator === '') {
+            return [];
+        }
+
+        $array = explode($separator, $text);
+
+        if (!$array) {
+            return [];
+        }
+
+        return $array;
+    }
+
+    /**
+     * Overwrites the file
+     *
+     * @param array  $fields
+     * @param string $separator
+     * @param string $enclosure
+     * @param string $escape_char
+     */
+    public function overwriteToFile(array $fields, string $separator = ",", string $enclosure = '"', string $escape_char = "\\"): void
+    {
+        $this->write('w+', $fields, $separator, $enclosure, $escape_char);
+    }
+
+    /**
+     * Append to the end of the file
+     *
+     * @param array  $fields
+     * @param string $separator
+     * @param string $enclosure
+     * @param string $escape_char
+     */
+    public function appendToFile(array $fields, string $separator = ",", string $enclosure = '"', string $escape_char = "\\"): void
+    {
+        $this->write('a+', $fields, $separator, $enclosure, $escape_char);
+    }
+
+    /**
+     * @param string $mode
+     * @param array  $fields
+     * @param string $separator
+     * @param string $enclosure
+     * @param string $escape_char
+     */
+    protected function write(string $mode, array $fields, string $separator = ",", string $enclosure = '"', string $escape_char = "\\"): void
+    {
+        $handle_file = $this->openFile($mode);
+
+        if (!$handle_file) {
+            return;
+        }
+
+        if ($fields === []) {
+            return;
+        }
+
+        foreach ($fields as $line_array) {
+            if (!is_array($line_array)) {
+                continue;
+            }
+
+            fputcsv($handle_file, $line_array, $separator, $enclosure, $escape_char);
+        }
+
+        fclose($handle_file);
+    }
+
+    /**
+     * @param string $mode
+     *
+     * @return false|resource
+     */
+    protected function openFile(string $mode = 'rb')
+    {
+        return fopen($this->path_to_file, $mode);
     }
 }
